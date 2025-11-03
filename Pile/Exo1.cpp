dae81fc02b76fb592f466414ffe6b1d5b5e3fdfd
tom-dvy <tomdavy49@gmail.com>
2025-11-03 11:29:22 +0100

    Exo 1 - Version finale

--- Pile/Exo1.cpp
+++ Pile/Exo1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-void StartTurn_A(int * remainingTurn, int * playerId);
-void SwitchPlayer_B(int * remainingTurn, int * playerId);
-void TurnLeft_C(int * remainingTurn, int * playerId);
+void StartTurn_A(int &remainingTurn, int &playerId);
+void SwitchPlayer_B(int &remainingTurn, int &playerId);
+void TurnLeft_C(int &remainingTurn, int &playerId);
 
 int main()
 {
     int remainingTurn = 100;
     int playerId = 0;
 
-    while (&remainingTurn > 0)
+    while (remainingTurn > 0)
     {
-        StartTurn_A(&remainingTurn, &playerId);
-        SwitchPlayer_B(&remainingTurn, &playerId);
-        TurnLeft_C(&remainingTurn, &playerId);
+        StartTurn_A(remainingTurn, playerId);
+        SwitchPlayer_B(remainingTurn, playerId);
+        TurnLeft_C(remainingTurn, playerId);
     }
     return 0;
 }
@@ -22,12 +22,13 @@ void StartTurn_A(int &remainingTurn, int &playerId)
 {
     std::cout << "Starting turn for player : " << playerId << "." << std::endl;
 }
+
 void SwitchPlayer_B(int &remainingTurn, int &playerId)
 {
-    std::cout << "Player " << playerId << " has played. Switching player."
-              << std::endl;
+    std::cout << "Player " << playerId << " has played. Switching player." << std::endl;
     playerId = (playerId + 1) % 2;
 }
+
 void TurnLeft_C(int &remainingTurn, int &playerId)
 {
     --remainingTurn;
