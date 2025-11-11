728435b2e966f1f5328afe3dc523559aea17a0e3
tom-dvy <tomdavy49@gmail.com>
2025-11-11 16:57:26 +0100

    VFinale ex 3

--- Tas/Exo3.cpp
+++ Tas/Exo3.cpp
@@ -4,23 +4,19 @@ int main()
 {
     for (int i = 1; i <= 5; i++)
     {
-        // allouer dynamiquement un tableau de taille i
         int * tab = new int[i];
 
-        // remplir chaque case avec sa valeur d'index
         for (int j = 0; j < i; ++j)
         {
             tab[j] = j;
         }
 
-        // afficher le contenu du tableau
         std::cout << "Taille = " << i << std::endl;
         for (int j = 0; j < i; ++j)
         {
             std::cout << tab[j] << std::endl;
         }
 
-        // désallouer
         delete[] tab;
     }
     
