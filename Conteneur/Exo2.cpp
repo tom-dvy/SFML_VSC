6055835fcf7a6d63ad4a72ea01cfcb73899c860a
tom-dvy <tomdavy49@gmail.com>
2025-11-19 10:22:53 +0100

    Exo 2 - Commentaires

--- Conteneur/Exo2.cpp
+++ Conteneur/Exo2.cpp
@@ -16,6 +16,7 @@ int main()
     std::map<std::string, int>::iterator found1 = AnimalsNames.find("Humain");
     std::map<std::string, int>::iterator found2 = AnimalsNames.find("Licorne");
 
+    std::cout << "Recherche d'humain et de licorne : " << std::endl;
     std::cout << "Nom : " << (found1 == AnimalsNames.end() ? "Non trouvé" : (*found1).first) << std::endl;
     std::cout << "Pattes : " << (found1 == AnimalsNames.end() ? 0 : (*found1).second) << std::endl;
 
@@ -25,10 +26,21 @@ int main()
     AnimalsNames.erase("Dodo");
 
     // Ajoute une patte au mouton
+    std::cout << "----------------------------" << std::endl;
+    std::cout << "Ajout d'une patte au mouton" << std::endl;
     std::map<std::string, int>::iterator found = AnimalsNames.find("Mouton");
     (*found).second++;
     std::cout << "Nom : " << (*found).first << std::endl;
     std::cout << "Pattes : " << (*found).second << std::endl;
+
+    std::cout << "----------------------------" << std::endl;
+    for (const auto &pair : AnimalsNames)
+    {
+        if (pair.second == 4)
+        {
+            std::cout << "Nom : " << pair.first << " Pattes : " << pair.second << std::endl;
+        }
+    }
 
     return 0;
 }
